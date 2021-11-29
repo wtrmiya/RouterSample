7486866dd20a2bbdf26e1ed7dddf619d88604403
Wataru Miyakoshi <wtrmiya@gmail.com>
2021-11-30 08:56:30 +0900

    add: tapButton() on SecondViewController
    First画面への遷移ボタンを追加した。

--- RouterSample/View/Second/SecondViewController.swift
+++ RouterSample/View/Second/SecondViewController.swift
@@ -7,24 +7,48 @@
 
 import UIKit
 final class SecondViewController:UIViewController {
-    var myLabel:UILabel = {
+    private var myLabel:UILabel = {
         let label = UILabel()
-        label.font = .systemFont(ofSize: 20)
-        label.text = "Second"
         label.textColor = .black
+        label.text = "Second"
         return label
+    }()
+    
+    private var myButton:UIButton = {
+        let button = UIButton()
+        button.setTitle("Firstへ遷移", for: UIControl.State.normal)
+        button.setTitleColor(UIColor.black, for: UIControl.State.normal)
+        button.addTarget(self, action: #selector(tapButton(_:)), for: UIControl.Event.touchUpInside)
+        return button
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        myLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        // viewの設定
         view.addSubview(myLabel)
         view.backgroundColor = .white
         
+        // ラベルの設定
+        myLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             myLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             myLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
         ])
+        
+        // ボタンの配置
+        view.addSubview(myButton)
+        myButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            myButton.topAnchor.constraint(equalTo: myLabel.bottomAnchor, constant: 50),
+            myButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+    
+    // 直接遷移
+    @objc func tapButton(_ sender:UIButton) {
+        let vc = UIStoryboard.firstViewController
+        vc.modalPresentationStyle = .fullScreen
+        self.present(vc, animated: true)
     }
 }
